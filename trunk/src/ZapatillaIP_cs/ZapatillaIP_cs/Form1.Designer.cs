7a84ef19532b9bc12d8908e06687f73b12486890
emaureir@gmail.com <emaureir@gmail.com@81c405d7-c6e4-8f5c-cf3a-63310799641c>
2012-01-15 23:10:58 +0000

    Correcciones a la forma de enviar mensajes.

--- trunk/src/ZapatillaIP_cs/ZapatillaIP_cs/Form1.Designer.cs
+++ trunk/src/ZapatillaIP_cs/ZapatillaIP_cs/Form1.Designer.cs
@@ -304,7 +304,7 @@ namespace ZapatillaIP_cs
             this.Controls.Add(this.tabControl1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "PDU Temp Compass";
             this.Shown += new System.EventHandler(this.Form1_Shown);
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
             this.tabControl1.ResumeLayout(false);
