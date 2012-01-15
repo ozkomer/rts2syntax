6b270274906394dde80f72a436a7ab671b905043
emaureir@gmail.com <emaureir@gmail.com@81c405d7-c6e4-8f5c-cf3a-63310799641c>
2012-01-15 15:30:10 +0000

    Se crea una interfaz con 16 botones, uno para cada relay. Las etiquetas de cada rele son configurables por el usuario.

--- src/ZapatillaIP_cs/ZapatillaIP_cs/Properties/Settings.Designer.cs
+++ src/ZapatillaIP_cs/ZapatillaIP_cs/Properties/Settings.Designer.cs
@@ -8,22 +8,210 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace ZapatillaIP_cs.Properties
-{
-
-
+namespace ZapatillaIP_cs.Properties {
+    
+    
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "8.0.0.0")]
-    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase
-    {
-
+    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
+        
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
-
-        public static Settings Default
-        {
-            get
-            {
+        
+        public static Settings Default {
+            get {
                 return defaultInstance;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("Baade")]
+        public string relay1Label {
+            get {
+                return ((string)(this["relay1Label"]));
+            }
+            set {
+                this["relay1Label"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("Zwicky")]
+        public string relay2Label {
+            get {
+                return ((string)(this["relay2Label"]));
+            }
+            set {
+                this["relay2Label"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("GTOCP3")]
+        public string relay3Label {
+            get {
+                return ((string)(this["relay3Label"]));
+            }
+            set {
+                this["relay3Label"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("ATC-02")]
+        public string relay4Label {
+            get {
+                return ((string)(this["relay4Label"]));
+            }
+            set {
+                this["relay4Label"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("Filter Wheel")]
+        public string relay5Label {
+            get {
+                return ((string)(this["relay5Label"]));
+            }
+            set {
+                this["relay5Label"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("Rotador")]
+        public string relay6Label {
+            get {
+                return ((string)(this["relay6Label"]));
+            }
+            set {
+                this["relay6Label"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("CCD")]
+        public string relay7Label {
+            get {
+                return ((string)(this["relay7Label"]));
+            }
+            set {
+                this["relay7Label"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("NE Camera")]
+        public string relay8Label {
+            get {
+                return ((string)(this["relay8Label"]));
+            }
+            set {
+                this["relay8Label"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("NW Camera")]
+        public string relay9Label {
+            get {
+                return ((string)(this["relay9Label"]));
+            }
+            set {
+                this["relay9Label"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("relay10Label")]
+        public string relay10Label {
+            get {
+                return ((string)(this["relay10Label"]));
+            }
+            set {
+                this["relay10Label"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("relay11Label")]
+        public string relay11Label {
+            get {
+                return ((string)(this["relay11Label"]));
+            }
+            set {
+                this["relay11Label"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("relay12Label")]
+        public string relay12Label {
+            get {
+                return ((string)(this["relay12Label"]));
+            }
+            set {
+                this["relay12Label"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("relay13Label")]
+        public string relay13Label {
+            get {
+                return ((string)(this["relay13Label"]));
+            }
+            set {
+                this["relay13Label"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("relay14Label")]
+        public string relay14Label {
+            get {
+                return ((string)(this["relay14Label"]));
+            }
+            set {
+                this["relay14Label"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("relay15Label")]
+        public string relay15Label {
+            get {
+                return ((string)(this["relay15Label"]));
+            }
+            set {
+                this["relay15Label"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("relay16Label")]
+        public string relay16Label {
+            get {
+                return ((string)(this["relay16Label"]));
+            }
+            set {
+                this["relay16Label"] = value;
             }
         }
     }
