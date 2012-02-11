56c0f28ad4bc828d297644459cd835f2f57de0c9
emaureir@gmail.com <emaureir@gmail.com@81c405d7-c6e4-8f5c-cf3a-63310799641c>
2012-02-11 05:21:42 +0000

    Modelo de los datos recibidos del arduino. Se modela el estado de los interruptores (Limits) y de los acelerometros.

---
+++ trunk/src/ZapatillaIP_cs/Serduino/Acelerometro.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Serduino
+{
+    public class Acelerometro
+    {
+        private double x;
+        private double y;
+        private double z;
+        public double maxX = Double.MinValue;
+        public double maxY = Double.MinValue;
+        public double maxZ = Double.MinValue;
+
+        public double minX = Double.MaxValue;
+        public double minY = Double.MaxValue;
+        public double minZ = Double.MaxValue;
+
+        public void setValues (double x,double y,double z)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+
+            if (this.x > maxX) maxX = this.x;
+            if (this.y > maxY) maxY = this.y;
+            if (this.z > maxZ) maxZ = this.z;
+
+            if (this.x < minX) minX = this.x;
+            if (this.y < minY) minY = this.y;
+            if (this.z < minZ) minZ = this.z;
+        }
+
+        public double X
+        {
+            get { return this.x; }
+            set { this.x = value; }
+        }
+        public double Y
+        {
+            get { return this.y; }
+            set { this.y = value; }
+        }
+        public double Z
+        {
+            get { return this.z; }
+            set { this.z = value; }
+        }
+
+        public int RelativeX
+        {
+            get { return (int)(100.0 * ((this.x - minX) / (maxX - minX))); }
+        }
+        public int RelativeY
+        {
+            get { return (int)(100.0 * ((this.y - minY) / (maxY - minY))); }
+        }
+        public int RelativeZ
+        {
+            get { return (int)(100.0 * ((this.z - minZ) / (maxZ - minZ))); }
+        }
+
+        /// <summary>
+        /// Calculos en base a Acelerometro.ods
+        /// </summary>
+        /// <param name="acc"></param>
+        /// <returns></returns>
+        public static double getAlpha(Acelerometro acc)
+        {
+            double c2, c3, d2, d3, e3, e5, f9;
+            c2 = 542; 
+            c3 = 81;
+            d2 = 554.5;
+            d3 = -33.5;
+            e3 = 87;
+            e5 = 510;
+            f9 = 1.35;//4.7123889804;
+            double alpha1, alpha2, alpha3;
+            double arg1, arg2, arg3;
+
+            #region alpha1
+            //Calculamos el argumento a entregar a la funcion arcoseno
+            arg1 = ((acc.x - c2) / (c3));
+            // aseguramos que el argumento esté en el rango de la funcion seno.
+            arg1 = Math.Max(-1, arg1);
+            arg1 = Math.Min(1, arg1);
+            // Calculamos el arcoseno
+            alpha1 = Math.Asin(arg1);
+            #endregion
+
+            #region alpha2
+            //Calculamos el argumento a entregar a la funcion arcoseno
+            arg2 = ((acc.y - d2) / (d3));
+            // aseguramos que el argumento esté en el rango de la funcion seno.
+            arg2 = Math.Max(-1, arg2);
+            arg2 = Math.Min(1, arg2);
+            // Calculamos el arcoseno
+            alpha2 = Math.Asin(arg2);
+            #endregion
+
+            #region alpha3
+            //Calculamos el argumento a entregar a la funcion arcoseno
+            arg3 = ((acc.z - e5) / (e3));
+            // aseguramos que el argumento esté en el rango de la funcion seno.
+            arg3 = Math.Max(-1, arg3);
+            arg3 = Math.Min(1, arg3);
+            // Calculamos el arcoseno
+            alpha3 = ((Math.Asin(arg3))+f9);
+            #endregion
+            Console.WriteLine("alfa123:\t" + alpha1 + "\t" + alpha2 + "\t" + alpha3);
+            return (0.9 * alpha1 + 0.1 * alpha2);
+        }
+    }
+}
---
+++ trunk/src/ZapatillaIP_cs/Serduino/Status.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Serduino
+{
+    public class Status
+    {
+        private String linea;
+        private int interruptores;
+        private static Acelerometro aRA=  new Acelerometro ();
+        private static Acelerometro aDEC = new Acelerometro();
+        private bool[] interruptor;
+        private bool raLimit1;
+        private bool raLimit2;
+        private bool raHome;
+        private bool decHome;
+
+
+        public Status(String linea)
+        {
+            this.linea = linea;
+            interruptor = new bool[3];
+        }
+
+        public Acelerometro AcelerometroRA
+        {
+            get { return aRA; }
+            set { aRA = value; }
+        }
+
+        public Acelerometro AcelerometroDEC
+        {
+            get { return aDEC; }
+            set { aDEC = value; }
+        }
+
+        public void Analiza()
+        {
+            String[] part;
+            part = this.linea.Split((" ").ToCharArray());
+            this.interruptores = Int16.Parse(part[0]);
+            if (part.Length >= 6)
+            {
+                aRA.setValues(Double.Parse(part[1]), Double.Parse(part[2]), Double.Parse(part[3]));
+                aDEC.setValues(Double.Parse(part[4]), Double.Parse(part[5]), Double.Parse(part[6]));
+            }
+            #region analisis interruptores
+            int valor;
+            valor = this.interruptores;
+
+            for (int i = 0; i < 3; i++)
+            {
+                this.interruptor[i] = ((valor % 2) == 1);
+                valor = (valor / 2);
+            }
+
+            this.decHome = interruptor[0];
+            this.raHome = (interruptor[1] & interruptor[2]);
+            if (!raHome)
+            {
+                this.raLimit1 = interruptor[1];
+                this.raLimit2 = interruptor[2];
+            }
+            #endregion
+
+            StringBuilder mensaje;
+            mensaje = new StringBuilder ();
+            mensaje.Append("interruptores="); mensaje.Append(this.interruptores);
+            mensaje.Append("\t decHome="); mensaje.Append(this.decHome);
+            mensaje.Append("\t raLimit1="); mensaje.Append(this.raLimit1);
+            mensaje.Append("\t raLimit2="); mensaje.Append(this.raLimit2);
+            mensaje.Append("\t raHome="); mensaje.Append(this.raHome);
+            Console.WriteLine(mensaje.ToString());
+
+        }
+    }
+}
