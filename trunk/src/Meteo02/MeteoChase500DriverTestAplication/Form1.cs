2c1974239c1ac606d9f18a65c0b78a0560ea5e94
emaureir@gmail.com <emaureir@gmail.com@81c405d7-c6e4-8f5c-cf3a-63310799641c>
2012-02-12 20:04:26 +0000

    Modificaciones para que la aplicación abra un block de notas y despliegue el último archivo de log.

--- trunk/src/Meteo02/MeteoChase500DriverTestAplication/Form1.cs
+++ trunk/src/Meteo02/MeteoChase500DriverTestAplication/Form1.cs
@@ -9,6 +9,7 @@ using System.Windows.Forms;
 using ASCOM.Meteo02;
 using log4net.Config;
 using log4net;
+using log4net.Appender;
 
 namespace MeteoChase500DriverTestAplication
 {
@@ -96,8 +97,94 @@ namespace MeteoChase500DriverTestAplication
 
         private void notifyIcon1_DoubleClick(object sender, EventArgs e)
         {
+            this.MostrarVentana();
+        }
+
+        private void showToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.MostrarVentana();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.ConfirmarClose();
+        }
+
+        private DialogResult ConfirmarClose()
+        {
+            DialogResult respuesta;
+            respuesta = MessageBox.Show("Closing application. Continue?", "Chase500 Meteorology Analisis.", MessageBoxButtons.YesNo);
+            if (respuesta == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+            return respuesta;
+        }
+
+        /// <summary>
+        /// Muestra la ventana de esta aplicacion.
+        /// </summary>
+        private void MostrarVentana()
+        {
             this.Show();
             this.WindowState = FormWindowState.Normal;
+            this.BringToFront();
         }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (e.CloseReason == CloseReason.ApplicationExitCall)
+            {
+                return;
+            }
+            DialogResult respuesta;
+            respuesta = this.ConfirmarClose();
+            if (respuesta == DialogResult.No)
+            {
+
+                e.Cancel = true;
+                this.WindowState = FormWindowState.Normal;
+            }
+        }
+
+        private void buttonShowLog_Click(object sender, EventArgs e)
+        {
+            RollingFileAppender rootAppender = (RollingFileAppender)((log4net.Repository.Hierarchy.Hierarchy)LogManager.GetRepository()).Root.Appenders[0];
+            string filename = rootAppender.File;
+            Console.WriteLine("filename=" + filename);
+            OpenLink(filename);
+        }
+
+        public void OpenLink(string sUrl)
+        {
+            try
+            {
+                System.Diagnostics.Process.Start(sUrl);
+            }
+            catch (Exception exc1)
+            {
+                // System.ComponentModel.Win32Exception is a known exception that occurs when Firefox is default browser.  
+                // It actually opens the browser but STILL throws this exception so we can just ignore it.  If not this exception,
+                // then attempt to open the URL in IE instead.
+                if (exc1.GetType().ToString() != "System.ComponentModel.Win32Exception")
+                {
+                    // sometimes throws exception so we have to just ignore
+                    // this is a common .NET bug that no one online really has a great reason for so now we just need to try to open
+                    // the URL using IE if we can.
+                    try
+                    {
+                        System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo("IExplore.exe", sUrl);
+                        System.Diagnostics.Process.Start(startInfo);
+                        startInfo = null;
+                    }
+                    catch (Exception exc2)
+                    {
+                        // still nothing we can do so just show the error to the user here.
+                    }
+                }
+            }
+        }
+
+
     }
 }
