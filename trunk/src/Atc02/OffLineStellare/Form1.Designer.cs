fb94959c9ba03d54ba089aa0fdd7f1f548b865af
emaureir@gmail.com <emaureir@gmail.com@81c405d7-c6e4-8f5c-cf3a-63310799641c>
2012-05-27 03:02:32 +0000

    Aplicación para monitorear archivos de log en modo Offline.

---
+++ trunk/src/Atc02/OffLineStellare/Form1.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace OffLineStellare
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.bOpen = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // bOpen
+            // 
+            this.bOpen.Location = new System.Drawing.Point(12, 12);
+            this.bOpen.Name = "bOpen";
+            this.bOpen.Size = new System.Drawing.Size(75, 23);
+            this.bOpen.TabIndex = 0;
+            this.bOpen.Text = "Open";
+            this.bOpen.UseVisualStyleBackColor = true;
+            this.bOpen.Click += new System.EventHandler(this.bOpen_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Filter = "Txt Files|*.log|All Files|*.*";
+            this.openFileDialog1.InitialDirectory = global::OffLineStellare.Properties.Settings.Default.OfficinaStellareLogFolder;
+            this.openFileDialog1.Title = "Officina Stelare Log file";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(603, 262);
+            this.Controls.Add(this.bOpen);
+            this.Name = "Form1";
+            this.Text = "Officina Stellare log Analizer";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button bOpen;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
