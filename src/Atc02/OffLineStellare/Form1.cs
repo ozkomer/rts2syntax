41236bae3e20cda1a082eb7a4552476631543b3f
emaureir@gmail.com <emaureir@gmail.com@81c405d7-c6e4-8f5c-cf3a-63310799641c>
2012-05-27 03:02:32 +0000

    Aplicación para monitorear archivos de log en modo Offline.

---
+++ src/Atc02/OffLineStellare/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using libOfflineLog;
+
+namespace OffLineStellare
+{
+    public partial class Form1 : Form
+    {
+        OfficinaStellareLog officinaStellareLog;
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.officinaStellareLog = null;
+        }
+
+        private void bOpen_Click(object sender, EventArgs e)
+        {           
+             this.openFileDialog1.ShowDialog(this);
+             String archivo;
+             archivo = this.openFileDialog1.FileName;
+             this.officinaStellareLog = new OfficinaStellareLog(archivo);
+             this.officinaStellareLog.analizar();
+             Console.WriteLine(this.officinaStellareLog.ToString());
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+    }
+}
