30e031ddc365b4e22e11fce078a6221e92477ca1
emaureir@gmail.com <emaureir@gmail.com@81c405d7-c6e4-8f5c-cf3a-63310799641c>
2012-05-27 02:52:17 +0000

    Aplicación de prueba para el driver alternativo del ATC02.

---
+++ src/Atc02/DriverTest/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using ASCOM.OrbitATC02;
+
+namespace DriverTest
+{
+    public partial class Form1 : Form
+    {
+        Focuser focus;
+        public Form1()
+        {
+            InitializeComponent();
+            focus = new Focuser();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bConnect_Click(object sender, EventArgs e)
+        {
+            focus.Connected = true;
+            Console.WriteLine("focus.Connected=" + focus.Connected);
+        }
+
+        private void bDisconnect_Click(object sender, EventArgs e)
+        {
+            focus.Connected = false;
+            Console.WriteLine("focus.Connected=" + focus.Connected);
+        }
+
+        private void bSetPosition_Click(object sender, EventArgs e)
+        {
+
+            bSetPosition.Text = "Position=" + focus.Position;
+            focus.Move( Int32.Parse(tbPosition.Text));
+            while(focus.IsMoving)
+            {
+
+                System.Threading.Thread.Sleep(200);
+            }
+            bSetPosition.Text = "Position=" + focus.Position;
+        }
+
+    }
+}
