599b5622119eab21f14c0aa74aed659274d6a389
emaureir@gmail.com <emaureir@gmail.com@81c405d7-c6e4-8f5c-cf3a-63310799641c>
2012-06-13 20:36:49 +0000

    Ahora, el arduino calcula los angulos "Distancia Zenital" y "CounterWeight".

--- trunk/src/serduino/serduino.ino
+++ trunk/src/serduino/serduino.ino
@@ -1,4 +1,4 @@
-/* 
+﻿/* 
  * Driver for Arduino used as multipurpose sensor.
  * Copyright (C) 2010 Petr Kubanek, Insitute of Physics <kubanek@fzu.cz>
  *
@@ -16,100 +16,157 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  */
- 
+
 #define SIZE_A  30
+
+
 
 // array for average values of sensors
 int avals[6][SIZE_A];
 double asums[6];
+double accel[6]; // RA (x,y,z) ; DEC (x,y,z) accelerations
+
 int avals_index = 0;
 int pinLimitRA=7;
 
+const double Zenith[]     = { 
+  -0.2116771931 , 0.975214028  , 0.0644233307 };
+const double SouthPole[]  = { 
+  0.3780225716  , 0.3874084811 , -0.84084101  };
+// Angulo entre el Zenith y la posición del tubo del telescopio. [en radianes].
+double zenithAngle;
+
+// Angulo del eje contrapeso. [en radianes].
+double counterWeightAngle;
+
 void setup()
 {
- // must switch to external reference
- analogReference(EXTERNAL);
-  
- Serial.begin(9600);
-  
- int i;
-  
- pinMode(pinLimitRA,OUTPUT);
- digitalWrite (pinLimitRA,LOW);
- 
- for (i = 8; i < 11; i++)
- {
-  pinMode(i, INPUT);
- }
-  
- // fill sensor array
- for (i = 0; i < 6; i++)
- {
-  asums[i] = 0;
- }
- for (i=0; i < SIZE_A; i++)
- {
-  for (int j = 0; j < 6; j++)
+  // must switch to external reference
+  analogReference(EXTERNAL);
+
+  Serial.begin(9600);
+
+  int i;
+
+  pinMode(pinLimitRA,OUTPUT);
+  digitalWrite (pinLimitRA,LOW);
+
+  for (i = 8; i < 11; i++)
   {
-   avals[j][i] = analogRead(j);
-   asums[j] += avals[j][i];
+    pinMode(i, INPUT);
   }
- }
- avals_index = 0;
+
+  // fill sensor array
+  for (i = 0; i < 6; i++)
+  {
+    asums[i] = 0;
+    accel[i]=0;
+  }
+  for (i=0; i < SIZE_A; i++)
+  {
+    for (int j = 0; j < 6; j++)
+    {
+      avals[j][i] = analogRead(j);
+      asums[j] += avals[j][i];
+    }
+  }
+  avals_index = 0;
+}
+
+void refreshCounterWeightAngle ()
+{
+  double cross;
+  cross =  ( (accel[0] * SouthPole[0]) + (accel[1] * SouthPole[1]) + (accel[2] * SouthPole[2]) );
+  counterWeightAngle = acos (cross); 
+}
+
+void refreshZenithAngle ()
+{
+  double cross;
+  cross =  ( (accel[3] * Zenith[0]) + (accel[4] * Zenith[1]) + (accel[5] * Zenith[2]) );
+  zenithAngle = acos (cross); 
+}
+
+
+void refreshAccelerations()
+{
+  static const double VZEROG = 1.5;//[Volts]
+  static const double VoltsPerCount = 0.0029325513196480938;//(VREF / MaxAnalogRead);//[volts]
+  static const double Sensitivity = 0.33;//0.55;//[Volt/g]
+  // Convertimos los voltajes de "AnalogRead" en Aceleraciones acordes con la especificacion del acelerometro
+  for (int i=0;i<6;i++)
+  {
+    accel[i] = (( (asums[i] / SIZE_A) * VoltsPerCount) - (VZEROG)) / Sensitivity;
+  }
+  // Normalizamos las aceleraciones convirtiendolas en vectores unitarios.
+  double cRoot;
+  cRoot =  sqrt((accel[0] * accel[0]) + (accel[1] * accel[1]) + (accel[2] * accel[2]));
+  accel[0] /= cRoot;
+  accel[1] /= cRoot;
+  accel[2] /= cRoot;
+  cRoot =  sqrt((accel[3] * accel[3]) + (accel[4] * accel[4]) + (accel[5] * accel[5]));
+  accel[3] /= cRoot;
+  accel[4] /= cRoot;
+  accel[5] /= cRoot;
 }
 
 void sendSensor()
 {
- int sensorValue = 0;
- int i;
- for (i = 8; i < 11; i++)
- {
-  sensorValue = sensorValue << 1;
-  sensorValue |= digitalRead(i);
- }
+  int sensorValue = 0;
+  int i;
+  for (i = 8; i < 11; i++)
+  {
+    sensorValue = sensorValue << 1;
+    sensorValue |= digitalRead(i);
+  }
 
- 
- Serial.print(sensorValue, DEC);
- Serial.print(" ");
-  
- for (i = 0; i < 6; i++)
- {
- 
-  Serial.print(asums[i] / SIZE_A, DEC);
+
+  Serial.print(sensorValue, DEC);
   Serial.print(" ");
- }
- Serial.println(" ");
+
+  for (i = 0; i < 6; i++)
+  {
+
+    Serial.print(asums[i] / SIZE_A, DEC);
+    Serial.print(" ");
+  }
+
+  refreshAccelerations();
+  refreshCounterWeightAngle ();
+  Serial.print(counterWeightAngle, DEC);
+  Serial.print(" ");
+  refreshZenithAngle ();
+  Serial.print(zenithAngle, DEC);
+
+  Serial.println();
 }
 
 void loop()
 {
- if  (digitalRead(8)==1)//  digitalRead(10)==1
- {
-   //sensorValue+=8;   
-   digitalWrite (pinLimitRA,HIGH);
- }
- if (Serial.available())
- {
-  char ch = Serial.read();
-  switch (ch)
+  if  (digitalRead(8)==1)//  Si se alcanza un LimitSwitch en RA
   {
-   case '?':
-    sendSensor();
-    break;
-   default:
-    digitalWrite (pinLimitRA,LOW);
-    Serial.println("E unknow command");
-  }   
- }
- for (int i = 0; i < 6; i++)
- {
-  asums[i] -= avals[i][avals_index];
-  int r = analogRead(i);
-  avals[i][avals_index] = r;
-  asums[i] += r;
- }
- avals_index ++;
- avals_index %= SIZE_A;
+    digitalWrite (pinLimitRA,HIGH); // Se informa al frigobar a traves del pinLimitRA
+  }
+  if (Serial.available())
+  {
+    char ch = Serial.read();
+    switch (ch)
+    {
+    case '?':
+      sendSensor();
+      break;
+    default:
+      digitalWrite (pinLimitRA,LOW);
+      Serial.println("E unknow command");
+    }   
+  }
+  for (int i = 0; i < 6; i++)
+  {
+    asums[i] -= avals[i][avals_index];
+    int r = analogRead(i);
+    avals[i][avals_index] = r;
+    asums[i] += r;
+  }
+  avals_index ++;
+  avals_index %= SIZE_A;
 }
-
-
