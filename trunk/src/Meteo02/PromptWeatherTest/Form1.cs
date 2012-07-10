55baa20235459461ee5f5a89e05e5e98174da324
emaureir@gmail.com <emaureir@gmail.com@81c405d7-c6e4-8f5c-cf3a-63310799641c>
2012-07-10 17:58:59 +0000

    Aplicación de prueba del driver ASCOM que monitorea los prompt.

---
+++ trunk/src/Meteo02/PromptWeatherTest/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using ASCOM.PromptWeather;
+
+namespace PromptWeatherTest
+{
+    public partial class Form1 : Form
+    {
+        private SafetyMonitor monitor;
+
+        public Form1()
+        {
+            monitor = new SafetyMonitor();
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            StringBuilder mensaje;
+            mensaje = new StringBuilder();
+            mensaje.AppendLine(DateTime.Now.ToShortDateString());
+            mensaje.Append("Prompt: Cant Domos Abiertos=");
+            mensaje.AppendLine(""+monitor.CantDomosAbiertos);
+            mensaje.Append("IsSafe=");
+            mensaje.Append(monitor.Safe);
+            Console.WriteLine(mensaje.ToString());
+        }
+    }
+}
