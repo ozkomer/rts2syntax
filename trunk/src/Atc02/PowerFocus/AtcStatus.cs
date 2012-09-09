8a808dd1b921bbbc337b7457bb35b02afa968ea1
emaureir@gmail.com <emaureir@gmail.com@81c405d7-c6e4-8f5c-cf3a-63310799641c>
2012-09-09 02:24:46 +0000

    Nuevo Proyecto PowerFocus. Ahora powerfocus maneja las comunicaciones seriales con el ATC02. Los clientes de esta aplicacion son el driver ASCOM focuser de Orbit, y FitsMonitor de Orbit. Estas aplicaciones se comunican con PowerFocus mediante breves sesiones TCP/IP.

---
+++ trunk/src/Atc02/PowerFocus/AtcStatus.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using log4net;
+
+namespace PowerFocus
+{
+    public class AtcStatus
+    {
+        private static readonly ILog logger = LogManager.GetLogger(typeof(PowerFocus.AtcStatus));
+
+        private double stabilizationTemperature;
+        private int fanPower;
+        private double primaryTemperature;
+        private double secondaryTemperature;
+
+        private double focusPosition;
+        private int shutterStatus;
+        private double ambientTemperature;
+        private double relativeHumidity;
+        private double pressure;
+        private double dewPoint;
+        private DateTime timeStamp;
+
+        /// <summary>
+        /// Crea un status a partir de la informacion cruda que envia el ATC02
+        /// </summary>
+        /// <param name="rawStatus"></param>
+        public AtcStatus()
+        {
+            this.stabilizationTemperature = Double.NaN;
+            this.fanPower = Int32.MinValue;
+            this.primaryTemperature = Double.NaN;
+            this.secondaryTemperature = Double.NaN;
+            this.focusPosition = Double.NaN;
+            this.shutterStatus = Int32.MinValue;
+            this.ambientTemperature = Double.NaN;
+            this.relativeHumidity = Double.NaN;
+            this.pressure = Double.NaN;
+            this.dewPoint = Double.NaN;
+            this.timeStamp = DateTime.MinValue;
+        }
+
+        public void refresh(String rawStatus, DateTime timestamp)
+        {
+            logger.Info( "inicio.");
+            this.timeStamp = timestamp;
+            String[] lineArray;
+            lineArray = rawStatus.Split(("\n").ToCharArray());
+            for (int i = 0; i < lineArray.Length; i++)
+            {
+                String line;
+                line = lineArray[i];
+                logger.Info( "Linea " + i + "=" + line);
+                if (line.StartsWith("STABT")) { this.stabilizationTemperature = extraeDouble(line); }
+                if (line.StartsWith("SETFAN")) { this.fanPower = extraeInt(line); }
+                if (line.StartsWith("PRITE")) { this.primaryTemperature = extraeDouble(line); }
+                if (line.StartsWith("SECTE")) { this.secondaryTemperature = extraeDouble(line); }
+                if (line.StartsWith("BFL")) { this.focusPosition = extraeDouble(line); }
+                if (line.StartsWith("SHUTTER")) { this.shutterStatus = extraeInt(line); }
+                if (line.StartsWith("AMBTE")) { this.ambientTemperature = extraeDouble(line); }
+                if (line.StartsWith("HUMID")) { this.relativeHumidity = extraeDouble(line); }
+                if (line.StartsWith("PRES")) { this.pressure = extraeDouble(line); }
+                if (line.StartsWith("DEWPO")) { this.dewPoint = extraeDouble(line.Replace("DEWPO", " ")); }
+            }
+            logger.Info("fin.");
+        }
+
+        public override string ToString()
+        {
+            StringBuilder respuesta;
+            respuesta = new StringBuilder();
+            respuesta.Append("<AtcStatus>");
+            respuesta.Append("<timeStamp>"); respuesta.Append(this.timeStamp.Ticks); respuesta.Append("</timeStamp>");
+            respuesta.Append("<stabilizationTemperature>"); respuesta.Append(this.stabilizationTemperature); respuesta.Append("</stabilizationTemperature>");
+            respuesta.AppendLine();
+            respuesta.Append("<fanPower>"); respuesta.Append(this.fanPower); respuesta.Append("</fanPower>");
+            respuesta.AppendLine();
+            respuesta.Append("<primaryTemperature>"); respuesta.Append(this.primaryTemperature); respuesta.Append("</primaryTemperature>");
+            respuesta.AppendLine();
+            respuesta.Append("<secondaryTemperature>"); respuesta.Append(this.secondaryTemperature); respuesta.Append("</secondaryTemperature>");
+            respuesta.AppendLine();
+            respuesta.Append("<focusPosition>"); respuesta.Append(this.focusPosition); respuesta.Append("</focusPosition>");
+            respuesta.AppendLine();
+            respuesta.Append("<shutterStatus>"); respuesta.Append(this.shutterStatus); respuesta.Append("</shutterStatus>");
+            respuesta.AppendLine();
+            respuesta.Append("<ambientTemperature>"); respuesta.Append(this.ambientTemperature); respuesta.Append("</ambientTemperature>");
+            respuesta.AppendLine();
+            respuesta.Append("<relativeHumidity>"); respuesta.Append(this.relativeHumidity); respuesta.Append("</relativeHumidity>"); 
+            respuesta.AppendLine();
+            respuesta.Append("<pressure>"); respuesta.Append(this.pressure); respuesta.Append("</pressure>");
+            respuesta.AppendLine();
+            respuesta.Append("<dewPoint>"); respuesta.Append(this.dewPoint); respuesta.Append("</dewPoint>");
+            respuesta.AppendLine();
+            respuesta.Append("</AtcStatus>");
+            return respuesta.ToString();
+        }
+
+        /// <summary>
+        /// Extrae un numero entero ubicado despues del último espacio del texto recibido
+        /// </summary>
+        /// <param name="linea">En este texto se busca el último elemento y se parsea como Int32</param>
+        /// <returns></returns>
+        private static int extraeInt(String linea)
+        {
+            String[] part;
+            int respuesta;
+            part = linea.Split((" ").ToCharArray());
+            respuesta = Int32.Parse(part[part.Length - 1]);
+            return respuesta;
+        }
+
+        /// <summary>
+        /// Extrae un numero entero ubicado despues del último espacio del texto recibido
+        /// </summary>
+        /// <param name="linea">En este texto se busca el último elemento y se parsea como Int32</param>
+        /// <returns></returns>
+        private static double extraeDouble(String linea)
+        {
+            String[] part;
+            double respuesta;
+            respuesta = Double.NaN;
+            //ASCOM.OrbitATC02.Focuser.sysLog.LogMessageCrLf("extraeDouble()", "linea=" + linea);
+
+            part = linea.Split((" ").ToCharArray());
+            try
+            {
+                respuesta = Double.Parse(part[part.Length - 1]);
+            }
+            catch (FormatException)
+            {
+                //ASCOM.OrbitATC02.Focuser.sysLog.LogMessageCrLf("extraeDouble(),FormatException", e.Message);
+            }
+            //ASCOM.OrbitATC02.Focuser.sysLog.LogMessageCrLf("extraeDouble()", "respuesta="+respuesta);
+            return respuesta;
+        }
+
+        public double StabilizationTemperature
+        {
+            get { return this.stabilizationTemperature; }
+            set { this.stabilizationTemperature = value; }
+        }
+
+        public int FanPower
+        {
+            get { return this.fanPower; }
+            set { this.fanPower = value; }
+        }
+
+        public double PrimaryTemperature
+        {
+            get { return this.primaryTemperature; }
+            set { this.primaryTemperature = value; }
+        }
+
+        public double SecondaryTemperature
+        {
+            get { return this.secondaryTemperature; }
+            set { this.secondaryTemperature = value; }
+        }
+
+
+        public double FocusPosition
+        {
+            get { return this.focusPosition; }
+            set { this.focusPosition = value; }
+        }
+
+        public int ShutterStatus
+        {
+            get { return this.shutterStatus; }
+            set { this.shutterStatus = value; }
+        }
+
+        public double AmbientTemperature
+        {
+            get { return this.ambientTemperature; }
+            set { this.ambientTemperature = value; }
+        }
+
+        public double RelativeHumidity
+        {
+            get { return this.relativeHumidity; }
+            set { this.relativeHumidity = value; }
+        }
+
+        public double Pressure
+        {
+            get { return this.pressure; }
+            set { this.pressure = value; }
+        }
+
+        public double DewPoint
+        {
+            get { return this.dewPoint; }
+            set { this.dewPoint = value; }
+        }
+
+
+
+    }
+}
---
+++ trunk/src/Atc02/PowerFocus/Form1.cs
@@ -0,0 +1,264 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net.Sockets;
+using System.Threading;
+using System.Net;
+using log4net;
+using log4net.Config;
+using log4net.Appender;
+
+namespace PowerFocus
+{
+    public partial class Form1 : Form
+    {
+        private static readonly ILog logger = LogManager.GetLogger(typeof(PowerFocus.Form1));
+
+        private TcpListener tcpListener;
+        private Thread listenThread;
+        private Boolean continueListen; 
+        private Atc02Comm focuser;
+
+        public Form1()
+        {
+            InitializeComponent();
+            XmlConfigurator.Configure();
+            logger.Info("Power Focus. Welcome!!!");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// http://www.switchonthecode.com/tutorials/csharp-tutorial-simple-threaded-tcp-server
+        /// </summary>
+        private void ListenForClients()
+        {
+
+            this.tcpListener.Start();
+
+            while (this.continueListen)
+            {
+                
+                //blocks until a client has connected to the server
+                TcpClient client;
+                client = null;
+                try
+                {
+                    client = this.tcpListener.AcceptTcpClient();
+                }
+                catch (SocketException e)
+                {
+                    Console.WriteLine(e.Message);
+                    return;
+                }
+                if (client != null)
+                {
+                    //create a thread to handle communication 
+                    //with connected client
+                    Thread clientThread = new Thread(new ParameterizedThreadStart(HandleClientComm));
+                    clientThread.Start(client);
+                }
+            }
+        }
+
+        private void HandleClientComm(object client)
+        {
+            TcpClient tcpClient = (TcpClient)client;
+            NetworkStream clientStream = tcpClient.GetStream();
+
+            byte[] message = new byte[4096];
+            int bytesRead;
+
+            while (true)
+            {
+                bytesRead = 0;
+
+                try
+                {
+                    //blocks until a client sends a message
+                    bytesRead = clientStream.Read(message, 0, 4096);
+                }
+                catch
+                {
+                    //a socket error has occured
+                    break;
+                }
+
+                if (bytesRead == 0)
+                {
+                    //the client has disconnected from the server
+                    break;
+                }
+
+                //message has successfully been received
+                ASCIIEncoding encoder = new ASCIIEncoding();
+                String mensajeRecibido;
+                String mensajeRespuesta;
+                mensajeRecibido = encoder.GetString(message, 0, bytesRead);
+                logger.Info("mensajeRecibido=" + mensajeRecibido);
+
+                #region respuesta al cliente
+                mensajeRespuesta = procesaMensaje(mensajeRecibido);
+                byte[] buffer = encoder.GetBytes(mensajeRespuesta);
+                clientStream.Write(buffer, 0, buffer.Length);
+                clientStream.Flush();
+                #endregion
+            }
+
+            tcpClient.Close();
+        }
+
+        private String procesaMensaje(String mensajeRecibido)
+        {
+            String mensajeRespuesta;             
+            mensajeRespuesta = "no implementado.";
+            if (mensajeRecibido.Equals("conectar"))
+            {
+
+                this.focuser.conectar();
+                mensajeRespuesta = ("" + this.focuser.Conectado);
+            }
+            if (mensajeRecibido.Equals("desconectar"))
+            {
+
+                this.focuser.desConectar();
+                mensajeRespuesta = ("" + this.focuser.Conectado);
+            }
+            if (mensajeRecibido.StartsWith("move"))
+            {
+                String[] part;
+                part = mensajeRecibido.Split( (" ").ToCharArray() );
+                int posicion;
+                posicion = Int32.Parse(part[1]);
+                this.focuser.Move(posicion);
+                mensajeRespuesta = (""+this.focuser.Posicion);
+            }
+            if (mensajeRecibido.Equals("GetXmlStatus"))
+            {
+                mensajeRespuesta = this.focuser.AtcStat.ToString();
+            }
+            if (mensajeRecibido.Equals("GetPosition"))
+            {
+                mensajeRespuesta = ""+this.focuser.Posicion;
+            }
+            if (mensajeRecibido.Equals("GetAmbientTemperature"))
+            {
+                mensajeRespuesta = ""+this.focuser.AtcStat.AmbientTemperature;
+            }
+            
+            if (mensajeRecibido.Equals("IsMoving"))
+            {
+                mensajeRespuesta = "" + this.focuser.EnMovimiento;
+            }
+            if (mensajeRecibido.Equals("IsConnected"))
+            {
+                mensajeRespuesta = "" + this.focuser.Conectado;
+            }
+
+            if (mensajeRecibido.Equals("ReadSettings"))
+            {
+                mensajeRespuesta = "" + this.focuser.ReadSettings();
+            }
+            if (mensajeRecibido.Equals("FindOptimal"))
+            {
+                mensajeRespuesta = "" + this.focuser.FindOptimal();
+            }
+            
+            if (mensajeRecibido.StartsWith("SetFan"))
+            {
+                String[] part;
+                part = mensajeRecibido.Split((" ").ToCharArray());
+                int fanspeed;
+                fanspeed = Int32.Parse(part[1]);
+
+                mensajeRespuesta = this.focuser.SetFan(fanspeed);
+            }
+            return mensajeRespuesta;
+
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (e.CloseReason == CloseReason.ApplicationExitCall)
+            {
+                return;
+            }
+            DialogResult respuesta;
+            respuesta = this.ConfirmarClose();
+            if (respuesta == DialogResult.No)
+            {
+
+                e.Cancel = true;
+                this.WindowState = FormWindowState.Normal;
+                return;
+            }
+
+            this.continueListen = false;
+            this.tcpListener.Stop();
+            while (this.listenThread.IsAlive)
+            {
+                Thread.Sleep(500);
+                logger.Info("Cerrando listener.");
+            }
+        }
+
+        PowerFocus.Properties.Settings settings = PowerFocus.Properties.Settings.Default;
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            this.focuser = Atc02Comm.Instance;
+            this.tcpListener = new TcpListener(IPAddress.Any, settings.FocusPort);
+            this.continueListen = true;
+            this.listenThread = new Thread(new ThreadStart(ListenForClients));
+            this.listenThread.Start();
+            this.focuser.conectar();
+        }
+
+        private void showToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.MostrarVentana();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.ConfirmarClose();
+        }
+
+        private DialogResult ConfirmarClose()
+        {
+            DialogResult respuesta;
+            respuesta = MessageBox.Show("Closing application. Continue?", "Focus Server", MessageBoxButtons.YesNo);
+            if (respuesta == DialogResult.Yes)
+            {
+                this.focuser.desConectar();
+                Application.Exit();
+            }
+            return respuesta;
+        }
+
+        /// <summary>
+        /// Muestra la ventana de esta aplicacion.
+        /// </summary>
+        private void MostrarVentana()
+        {
+            this.Show();
+            this.WindowState = FormWindowState.Normal;
+            this.BringToFront();
+        }
+            
+        
+    }
+}
